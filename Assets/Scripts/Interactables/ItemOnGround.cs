ab52e3fbc37d65e91c5f51bbb0dfb2fdf86887e2
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-08-31 23:59:53 +0300

    Refactor ItemOnGround

--- Assets/Scripts/Interactables/ItemOnGround.cs
+++ Assets/Scripts/Interactables/ItemOnGround.cs
@@ -5,30 +5,15 @@ using UnityEngine;
 public class ItemOnGround : Interactable
 {
     public Item item;
-    public Vector3 rotation = new Vector3(0, -1, 0);
-    
-    private GameObject player;
 
-    private void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-        player = players[0];
-    }
-
-    private void Update()
-    {
-        if (wasCollidedWithPlayer)
+        if (other.CompareTag("Player"))
         {
-            Inventory playerInventory = player.GetComponent<Inventory>();
-            bool wasPickedUp = playerInventory.Add(item);
+            bool wasPickedUp = other.GetComponent<Inventory>().Add(item);
 
             if (wasPickedUp)
                 Destroy(gameObject);
         }
-    }
-
-    private void FixedUpdate()
-    {
-        transform.Rotate(rotation);
     }
 }
---
+++ Assets/Scripts/Interactables/TurnOnTheFloor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnTheFloor : MonoBehaviour
+{
+    public Vector3 rotation = new Vector3(0, -1, 0);
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(rotation);
+    }
+}
