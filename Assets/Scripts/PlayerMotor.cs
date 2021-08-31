b247f6a7045d9452cee422276ccee7d37b4df3ff
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-08-31 23:31:31 +0300

    Refactor PlayerMotor script

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -4,47 +4,47 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
-    public CharacterController controller;
-    Transform mainCamera;
-    public Animator animator;
+    private CharacterController controller;
+    private Transform mainCamera;
+    private Animator animator;
 
     public float speed = 6f;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-
     Vector3 velocity;
-    bool isGrounded;
 
     public float gravity = -9.81f;
     public float jumpheight = 1f;
 
     private void Start()
     {
+        controller = GetComponent<CharacterController>();
         animator = GetComponentInChildren<Animator>();
         mainCamera = Camera.main.transform;
     }
 
     void Update()
     {
+        Walk();
+        Jump();
+        Falling();
+    }
+
+    private void Walk()
+    {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
-        Vector3 target = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+        Vector3 targetDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
 
-        if (target.magnitude >= 0.1f)
+        if (targetDirection.magnitude >= 0.1f)
         {
-            // rotation
-            float targetAngle = Mathf.Atan2(target.x, target.z) * Mathf.Rad2Deg;
-            float targetAngleByCamera = targetAngle + mainCamera.eulerAngles.y;
-            float smoothedAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngleByCamera, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, smoothedAngle, 0f);
+            float smoothedTurnAngle = GetSmoothedTurnAngle(targetDirection);
 
-            // horizontal movement
-            Vector3 moveTarget = Quaternion.Euler(0f, targetAngleByCamera, 0f) * Vector3.forward;
+            transform.rotation = Quaternion.Euler(0f, smoothedTurnAngle, 0f);
+
+            Vector3 moveTarget = Quaternion.Euler(0f, smoothedTurnAngle, 0f) * Vector3.forward;
             controller.Move(moveTarget.normalized * speed * Time.deltaTime);
 
             animator.SetBool("Walking", true);
@@ -53,18 +53,27 @@ public class PlayerMotor : MonoBehaviour
         {
             animator.SetBool("Walking", false);
         }
+    }
 
-        // jump
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if (Input.GetButtonDown("Jump") && isGrounded)
+    private float GetSmoothedTurnAngle(Vector3 targetDirection)
+    {
+        float targetAngle = Mathf.Atan2(targetDirection.x, targetDirection.z) * Mathf.Rad2Deg;
+        float targetAngleByCamera = targetAngle + mainCamera.eulerAngles.y;
+        float smoothedTurnAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngleByCamera, ref turnSmoothVelocity, turnSmoothTime);
+        return smoothedTurnAngle;
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpheight * -2f * gravity);
         }
+    }
 
-        // gravity
+    private void Falling()
+    {
         velocity.y += gravity * Time.deltaTime;
-
-        // vertical movement
         controller.Move(velocity * Time.deltaTime);
     }
 }
