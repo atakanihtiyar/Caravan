e394716abe75df411ecf94a36be139e46fd02ee9
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-09-15 01:36:45 +0300

    Create character stat script

---
+++ Assets/1. Character Stat Sysytem/Scripts/ScriptableObjects/CharacterStats_SO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New stat", menuName = "Character/Stat")]
+public class CharacterStats_SO : ScriptableObject
+{
+
+}
