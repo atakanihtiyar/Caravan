8b4fc5fd12a5df5c416c539a24b03e310721abb4
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-09-15 01:50:29 +0300

    Define variables on character stats

--- Assets/1. Character Stat Sysytem/Scripts/ScriptableObjects/CharacterStats_SO.cs
+++ Assets/1. Character Stat Sysytem/Scripts/ScriptableObjects/CharacterStats_SO.cs
@@ -5,5 +5,39 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New stat", menuName = "Character/Stat")]
 public class CharacterStats_SO : ScriptableObject
 {
+    #region Fields
 
+    public bool setManully = false;
+    public bool saveDataOnClose = false;
+
+    public ItemPickUp weapon { get; private set; }
+    public ItemPickUp headArmor { get; private set; }
+    public ItemPickUp chestArmor { get; private set; }
+    public ItemPickUp handArmor { get; private set; }
+    public ItemPickUp legArmor { get; private set; }
+    public ItemPickUp footArmor { get; private set; }
+    public ItemPickUp misc1 { get; private set; }
+    public ItemPickUp misc2 { get; private set; }
+
+    public int maxHealth = 0;
+    public int currentHealth = 0;
+
+    public int maxStamina = 0;
+    public int currentStamina = 0;
+
+    public int maxWealth = 0;
+    public int currentWealth = 0;
+
+    public int baseDamage = 0;
+    public int currentDamage = 0;
+
+    public float baseResistance = 0;
+    public float currentResistance = 0;
+
+    public float currentEmcumbrance = 0f;
+
+    public int charExperience = 0;
+    public int charLevel = 0;
+
+    #endregion
 }
